cce17fb81348959385559aaa653fed7f2433ab93
NikhilMali <nikhilpundalikmali@gmail.com>
2026-06-29 19:59:21 +0530

    Implicit animations: AnimatedContainer, AnimatedOpacity

---
+++ lib/screens/MyHomePage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Home Page',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePageScreen(),
+    );
+  }
+}
+
+class MyHomePageScreen extends StatefulWidget {
+  const MyHomePageScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePageScreen> createState() => _MyHomePageScreenState();
+}
+
+class _MyHomePageScreenState extends State<MyHomePageScreen> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Home Page')),
+      body: Center(
+        child: GestureDetector.new(
+          onTap: () => setState(() {
+            _isExpanded = !_isExpanded;
+          }),
+          child: AnimatedOpacity(
+            opacity: _isExpanded ? 1.0 : 0.1,
+            duration: const Duration(milliseconds: 300),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              width: _isExpanded ? 200 : 100,
+              height: _isExpanded ? 200 : 100,
+              color: _isExpanded ? Colors.blue : Colors.red,
+              child: const Center(
+                child: Text(
+                  'Tap me!',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
